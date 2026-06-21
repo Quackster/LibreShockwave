2b319b1c2871af47f25b580a96476fd2557776fe
Quackster <quacksterr.15@gmail.com>
2026-06-21 12:21:43 +1000

    Reduce parameter stack copy overhead

--- cpp/src/lingo/vm/Scope.cpp
+++ cpp/src/lingo/vm/Scope.cpp
@@ -112,7 +112,7 @@ void Scope::pushParam(int index) {
     if (index >= 0 && index < static_cast<int>(modifiedParams_.size())) {
         const auto& modified = modifiedParams_[static_cast<std::size_t>(index)];
         if (modified) {
-            push(*modified);
+            pushCopy(*modified);
             return;
         }
     }
