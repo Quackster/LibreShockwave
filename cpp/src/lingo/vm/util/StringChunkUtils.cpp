afda4036f32195ee9a30f0bbaa26342c02488624
Quackster <quacksterr.15@gmail.com>
2026-06-21 14:55:18 +1000

    Emplace split chunk strings

--- cpp/src/lingo/vm/util/StringChunkUtils.cpp
+++ cpp/src/lingo/vm/util/StringChunkUtils.cpp
@@ -13,7 +13,7 @@ std::vector<std::string> splitWords(std::string_view value) {
     for (char ch : value) {
         if (static_cast<unsigned char>(ch) <= static_cast<unsigned char>(' ')) {
             if (!current.empty()) {
-                words.push_back(std::move(current));
+                words.emplace_back(std::move(current));
                 current.clear();
             }
         } else {
@@ -21,7 +21,7 @@ std::vector<std::string> splitWords(std::string_view value) {
         }
     }
     if (!current.empty()) {
-        words.push_back(std::move(current));
+        words.emplace_back(std::move(current));
     }
     return words;
 }
@@ -55,13 +55,13 @@ std::vector<std::string> splitItems(std::string_view value, char delimiter) {
     std::string current;
     for (char ch : value) {
         if (ch == delimiter) {
-            items.push_back(std::move(current));
+            items.emplace_back(std::move(current));
             current.clear();
         } else {
             current.push_back(ch);
         }
     }
-    items.push_back(std::move(current));
+    items.emplace_back(std::move(current));
     return items;
 }
 
